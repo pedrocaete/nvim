da1ef5b6b84561df29eb019e378225a880408b87
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2024-07-06 11:21:02 -0300

    Keymap to view all keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,11 +2,13 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+--Open keymaps
+keymap.set("n", "<leader>mm", ":edit ~/.config/nvim/lua/core/keymaps.lua<CR>")
 --Best map to save and exit
 keymap.set("n", "<S-w><S-w>", "<Esc>:w<CR>")
 keymap.set("i", "<C-s>", "<Esc>:w<CR>")
 keymap.set("n", "<S-q><S-q>", "<Esc>:confirm q<CR>")
---
+
 --Centralize when move
 keymap.set("n", "j", "jzz")
 keymap.set("n", "k", "kzz")
