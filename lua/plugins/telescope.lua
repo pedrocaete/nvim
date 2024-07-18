1d8667e3fda84587e3ffd3ddb948d3b12bb08f3a
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2024-07-18 14:31:48 -0300

    Keymap corrected

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,6 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
   end
   },
   {
