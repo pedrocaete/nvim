776f0f3cfff09a609d157c4fc8ee7131b7c9e46f
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2024-07-21 14:10:05 -0300

    Troubleshotting

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,9 @@
-return{
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    vim.opt.termguicolors = true
-    require("bufferline").setup({})
-    vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', {})
-    vim.api.nvim_set_keymap('n', '<leader>x', ':bd!<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<Leader>b', ':enew<CR>', { noremap = true, silent = true })
-  end
+return {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        vim.opt.termguicolors = true
+        require("bufferline").setup({})
+    end,
 }
