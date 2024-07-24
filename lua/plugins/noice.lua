2e45533ac0f3b58207a56f474a544ace0b8c1a11
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2024-07-23 22:54:28 -0300

    Feature: Noice Plugin

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/noice.nvim",
+	config = function()
+		require("noice").setup({
+			-- add any options here
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+							{ find = "%d fewer lines" },
+							{ find = "%d more lines" },
+						},
+					},
+					opts = { skip = true },
+				},
+			},
+		})
+		require("notify").setup({
+			background_colour = "#aaaaaa",
+		})
+	end,
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+}
