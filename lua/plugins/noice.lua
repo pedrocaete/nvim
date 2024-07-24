9a1fcd594880f4f24c99e3e0fcfd6d2af1ef371b
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2024-07-24 15:06:56 -0300

    Fixed: Macros recoriding on noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,30 +1,37 @@
 return {
-	"folke/noice.nvim",
-	config = function()
-		require("noice").setup({
-			-- add any options here
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "%d+L, %d+B" },
-							{ find = "; after #%d+" },
-							{ find = "; before #%d+" },
-							{ find = "%d fewer lines" },
-							{ find = "%d more lines" },
-						},
-					},
-					opts = { skip = true },
-				},
-			},
-		})
-		require("notify").setup({
-			background_colour = "#aaaaaa",
-		})
-	end,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
+    "folke/noice.nvim",
+    config = function()
+        require("noice").setup({
+            -- add any options here
+            routes = {
+--                {
+--                    filter = {
+--                        event = "msg_show",
+--                        any = {
+--                            { find = "%d+L, %d+B" },
+--                            { find = "; after #%d+" },
+--                            { find = "; before #%d+" },
+--                            { find = "%d fewer lines" },
+--                            { find = "%d more lines" },
+--                        },
+--                    },
+--                   opts = { skip = true },
+--                },
+                {
+                    view = "notify",
+                    filter = {
+                        event = "msg_showmode",
+                        find = "recording",
+                    },
+                },
+            },
+        })
+        require("notify").setup({
+            background_colour = "#aaaaaa",
+        })
+    end,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
 }
