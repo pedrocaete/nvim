02e88371c67c746fdd5e891c0e2c9de693d66bcd
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-09-19 17:59:43 -0300

    .

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -33,9 +33,9 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<Right>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["Up"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["Down"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
