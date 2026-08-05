b2b0b2152328dfd2567e8d3a50bab70178ccda67
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2026-08-05 11:08:19 -0300

    cpp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,12 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.csharpier,
+				null_ls.builtins.formatting.clang_format.with({
+					filetypes = { "c", "cpp", "cuda" },
+					extra_args = {
+						"--style={BasedOnStyle: LLVM, UseTab: Never, IndentWidth: 4, TabWidth: 4}",
+					},
+				}),
 
 				-- Carregamos o plugin aqui dentro para evitar o erro de loop
 				null_ls.builtins.diagnostics.phpcs.with(require("wordpress").null_ls_phpcs),
@@ -16,6 +22,22 @@ return {
 		})
 
 		-- Atalho para formatar
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gf", function()
+			local clang_format_filetypes = {
+				c = true,
+				cpp = true,
+				cuda = true,
+			}
+
+			vim.lsp.buf.format({
+				filter = function(client)
+					if clang_format_filetypes[vim.bo.filetype] then
+						return client.name == "null-ls"
+					end
+
+					return true
+				end,
+			})
+		end, {})
 	end,
 }
